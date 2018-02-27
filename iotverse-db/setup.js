6fb0d84d1220cdec34bacc36036491d3503f8eeb
3lv27 <elvin.gm27@gmail.com>
2018-02-27 23:11:58 +0100

    setup file

---
+++ iotverse-db/setup.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const debug = require('debug')('iotverse:db:setup') // where we are debugging
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')
+
+const prompt = inquirer.createPromptModule()
+
+async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will detsroy your database, are you sure?',
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+  
+  const config = {
+    database: process.env.DB_NAME || 'iotverse',
+    username: process.env.DB_USER || 'musk',
+    password: process.env.DB_PASS || 'none', //no password needed due to pg_hba.conf, if you want to require password, switch the config to password instead of trust
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Succes!')
+  process.exit(0)
+
+}
+
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+
+setup()
