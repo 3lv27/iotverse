cda82530c3733f8ffa89d13b0507b162b7b9b0bc
3lv27 <elvin.gm27@gmail.com>
2018-02-27 22:45:28 +0100

    setting up the dbs

--- iotverse-db/index.js
+++ iotverse-db/index.js
@@ -9,10 +9,17 @@ module.exports = async function (config) {
   const AgentModel = setupAgentModel(config)
   const MetricModel = setupMetricModel(config)
 
+  // establishing the relationshio between our dbs
   AgentModel.hasMany(MetricModel)
   MetricModel.belongsTo(AgentModel)
 
+  // it queries the db to test the connection
   await sequelize.authenticate()
+
+  // if we have a config.setup ready, we are gonna create a db forcing it. If there was a previous db it will be erased
+  if (config.setup) {
+    await sequelize.sync({force: true})
+  }
 
   const Agent = {}
   const Metric = {}
