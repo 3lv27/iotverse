baab81673135fe8a2ff3527b2652496fc34d70bd
3lv27 <elvin.gm27@gmail.com>
2018-03-02 20:05:50 +0100

    code coverage, sinon and proxyquire added

--- iotverse-db/tests/agent-test.js
+++ iotverse-db/tests/agent-test.js
@@ -1,20 +1,49 @@
 'use strict'
 
-const test = require('ava');
+const test = require('ava')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
 
 let config = {
   logging: function () {}
 }
 
+let MetricStub = { //it represents (mockup) our Metric model
+  belongsTo: sinon.spy() // this function allos you to ask many things to our db i.e. how many times it was called
+
+}
+
+let AgentStub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
-  const setupDatabase = require('../')
+  sandbox = sinon.sandbox.create() // We need a sandbox in order to replicate the environment because the agentstub isnt in the global scope 
+
+  AgentStub = { //mockup Agent model
+    hasMany: sandbox.spy() // spy from the sandbox
+  }
+
+  const setupDatabase = proxyquire('../', {
+    './models/agent': () => AgentStub,
+    './models/metric': () => MetricStub,
+
+  })
   db = await setupDatabase(config)
+})
+
+test.afterEach(() => {
+  sandbox && sinon.sandbox.restore()
 })
 
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent services should exist')
 })
 
-//nothing
+test.serial('Setup', t => {
+  t.true(AgentStub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
