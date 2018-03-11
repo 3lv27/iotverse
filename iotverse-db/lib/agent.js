167de79e07fa925d97233f0614672e9832679af6
3lv27 <elvin.gm27@gmail.com>
2018-03-11 10:35:06 +0100

    createOrUpdate stub  done

--- iotverse-db/lib/agent.js
+++ iotverse-db/lib/agent.js
@@ -1,11 +1,31 @@
 'use strict'
 
 module.exports = function setupAgent (AgentModel) {
+  async function createOrUpdate (agent) {
+    const cond = {
+      where: {
+        uuid: agent.uuid
+      }
+    }
+
+    const existignAgent = await AgentModel.findOne(cond)
+
+    if(existignAgent) {
+      const updated = await AgentModel.update(agent, cond)
+      return updated ? AgentModel.findOne(cond) : existignAgent
+    }
+
+    const result = await AgentModel.create(agent)
+    return result.toJSON()
+  }
+
+
   function findById (id) {
     return AgentModel.findById(id)
   }
 
   return {
+    createOrUpdate,
     findById
   }
 }
--- iotverse-db/tests/agent-test.js
+++ iotverse-db/tests/agent-test.js
@@ -7,7 +7,7 @@ const proxyquire = require('proxyquire')
 const agentFixtures = require('./fixtures/agent')
 
 let config = {
-  logging: function () {}
+  logging () {}
 }
 
 let MetricStub = { //it represents (mockup) our Metric model
@@ -17,9 +17,16 @@ let MetricStub = { //it represents (mockup) our Metric model
 
 let single = Object.assign({}, agentFixtures.single)
 let id = 1
+let uuid = 'yyy-yyy-yyy'
 let AgentStub = null
 let db = null
 let sandbox = null
+
+let uuidArgs = {
+  where: {
+    uuid
+  }
+}
 
 test.beforeEach(async () => {
   sandbox = sinon.sandbox.create() // We need a sandbox in order to replicate the environment because the agentstub isnt in the global scope 
@@ -28,9 +35,17 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy() // spy from the sandbox
   }
 
-  // Model findByID Stub
+  // Model findOne Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+
+  // Model findById Stub
   AgentStub.findById = sandbox.stub()
   AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+
+  //Model Update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
 
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
@@ -63,5 +78,18 @@ test.serial('Agent#findById', async t => {
   t.true(AgentStub.findById.calledWith(id), 'findById should be called with specified id')
 
   t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'findOne should be called on model')
+  t.true(AgentStub.findOne.calledTwice, 'findOne should be called twice')
+  t.true(AgentStub.findOne.calledWith(uuidArgs), 'findOne should be called with uuid args')
+  t.true(AgentStub.update.called, 'agent.update should be called on model')
+  t.true(AgentStub.update.calledOnce, 'agent.update should be called once')
+  t.true(AgentStub.update.calledWith(single), 'agent.update should be called with specified args')
+
+  t.deepEqual(agent, single, 'agent should be the same')
 })
 
